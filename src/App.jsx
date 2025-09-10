ff94598acfe15493a02cb260b78d582c398b36bc
Adithya445 <adithya.ananthanarayanan@kgpian.iitkgp.ac.in>
2025-09-10 19:20:48 +0530

    Dashboard, Audit and KPIs added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import './App.css'
 import Login from './Components/Authentication/Login'
 import Signup from './Components/Authentication/Signup'
 import LiveStatus from './Components/Live Platfrom Status/LiveStatus'
-
+import Audit from './Components/Output/Audit'
+import Dashboard  from './Components/Output/Dashboard'
+import KPI from './Components/Output/KPI'
 function App() {
 
   return (
     <>
       {/* <Login /> */}
       {/* <Signup /> */}
-      <LiveStatus/>
+      <KPI/>
     </>
   )
 }
